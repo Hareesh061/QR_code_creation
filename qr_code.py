f18518c7d8af35e4a533ec1d1e1dd4725bd250a9
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2023-10-22 09:52:02 +0530

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -1,3 +1,3 @@
 import qrcode as qr
-img=qr.make("https://drive.google.com/file/d/172L6DLYkXk6ex4fc5tAg3YwGn-nQEd6K/view?usp=sharing")
-img.save("Resume.png")
+img=qr.make("enter your link path here")
+img.save("give your choosen name here")
